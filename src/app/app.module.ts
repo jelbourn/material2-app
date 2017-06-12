699283eebea71e70420f5e97ee20c6412c821d56
Jeremy Elbourn <jelbourn@google.com>
2017-06-12 15:39:50 -0700

    bad error message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,58 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule, Component} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {MdCheckboxModule} from '@angular/material';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+
+
+
+@Component({
+  selector: 'x-home',
+  template: 'Home',
+})
+export class Home { }
+
+
+@Component({
+  selector: 'x-other',
+
+  // !!! If `<md-checkbox>` is removed from this template, the route
+  //     works even though `OtherModule` is never imported.
+  template: 'Other page <md-checkbox>Check</md-checkbox>'
+})
+export class Other { }
+
+
+@NgModule({
+  imports: [MdCheckboxModule],
+  declarations: [Other]
+})
+export class OtherModule { }
+
+
+export const routeConfig: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: Home},
+
+  // !!! This route is routed to without its NgModule being imported.
+  {path: 'other', component: Other},
+];
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Home,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routeConfig),
+    // Intentionally not including OtherModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
